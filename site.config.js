32adc58abc59cb0f0f47cdfaec1c5495759f1383
hhh123555000 <73821314+hhh123555000@users.noreply.github.com>
2021-09-17 19:14:37 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: 'Ling-s-601620cd25a54a90b0e9def2fd58d8ab',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "Ling's Blogs",
+  domain: "Ling's Blogs",
+  author: "Ling's",
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: "Ling's Blogs",
+  socialImageTitle: "Ling's Blogs",
+  socialImageSubtitle: "Ling's Blogs 👋",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  github: 'hhh123555000',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
